827858fb040ebfa1a2055baa6fb4344f3a1d7377
rahmagamal <55654504+rahmagamal@users.noreply.github.com>
2022-01-26 21:30:04 +0200

    first commit

---
+++ js/owlCarousel.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+       
+        autoplay:true,
+        autoplayTimeout:3000,
+        // autoplayHoverPause:true,
+    
+    items : 1,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [979,3],
+    center: true,
+    nav:true,
+    // dots: true, 
+    
+    responsive: {
+    600: {
+    items: 1
+    }
+    }
+    
+    
+    
+    });
+    
+    });
+
+
+  
+    
